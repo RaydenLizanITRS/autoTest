dab024c2e683d669b09f831df40fb19f4ec07106
RaydenLizanITRS <rlizan@itrsgroup.com>
2024-11-22 11:38:51 +0800

    2024-11-22 11:38:50

--- daily_update.sh
+++ daily_update.sh
@@ -7,22 +7,20 @@ COMMIT_MESSAGE=$(date "+%Y-%m-%d %H:%M:%S")
 
 
 
-# Switch to the specified branch
+# Switch to the branch
 git checkout "$BRANCH"
 git fetch origin "$BRANCH"
 
 git add .
 
-
+#Commit and push if there are any changes
 if [[ -n $(git status --porcelain) ]]; then
 	git commit -m "$COMMIT_MESSAGE"
 	git push origin "$BRANCH"
+	echo "Changes committed and pushed successfully to the $BRANCH branch."
 else
 	echo "No changes to commit, skipping push."
-	end
 fi
 
-
-echo "Changes committed and pushed successfully to the $BRANCH branch."
 
 
